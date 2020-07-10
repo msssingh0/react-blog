dd5293fc9c9dbc17048b70bdcc4b596c7b2e081b
mss <singh7179@gmail.com>
2020-07-10 21:56:07 +0700

    db details

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:abcd1234@blog.h3ah2.mongodb.net/sample_airbnb?retryWrites=true&w=majority',
-{useNewUrlParser:true})
-.then(() => console.log('Db connected'))
-.catch(err => console.log('error:',err));
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,{useNewUrlParser:true})
+    .then(() => console.log('Db connected'))
+    .catch(err => console.log('error:',err));
 
 app.get('/',(req,res) => {
     res.send("Hello Manish");
+});
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    console.log(req.body);
+    const user = new User(req.body);
+    
+    user.save((err,userData) => {
+        if(err){
+            return res.json({success:false,err});
+        }
+        return res.json({success:true}).status(200);
+    });
 });
 
 app.listen(5000);
